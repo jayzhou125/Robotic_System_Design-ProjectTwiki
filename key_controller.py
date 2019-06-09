3c4c2f0219e1d8b73ec88defd34dec0e0fa7fa63
jayzhou125 <33584805+jayzhou125@users.noreply.github.com>
2019-06-08 20:38:49 -0400

    Update key_controller.py

--- key_controller.py
+++ key_controller.py
@@ -14,27 +14,71 @@ import odom # is this how we connect odom?
 
 pub_ctrl = rospy.Publisher("/kobuki_command", Twist, queue_size=10)	# publisher kobuki_command
 pub_stop = rospy.Publisher("/emergency_stop", Empty, queue_size=10)	# publisher stop
+# pub_resume = rospy.Publisher("/resume", Empty, queue_size=10)
 
-global targetX, targetZ
+targetX, targetZ = 0.0
+keyPressed = None
+command = Twist()
+dirty = False
+# no_accel = True
+
+def cleanUp():
+    global pub_ctrl, command
+    command.linear.x = 0.0
+    command.angular.z = 0.0
+    pub_ctrl.publish(command)
+    rospy.sleep(1)
 
 def keyCallback(data)
+    global keyPressed, dirty
     keyPressed = data
+    print data
+    dirty = True
+
+def update_command()
+    global pub_ctrl, command, keyPressed, dirty, targetX, targetZ
+	
+    pub_ctrl.publish(command)
+	
+    # stop
     if keyPressed == STOP:	# stop the robot
-    	pub_stop.publish(Empty())	
-    elif keyPressed == UP：
-		# move forward with constant_command.py
-		# something like .... constant_command.publish(0)
-		# as button is pressed, accelerate
-		command.linear.x = SHIFT
-    elif keyPressed == DOWN:
-		# move backwards
-		command.linear.x = -1 * SHIFT
-    elif keyPressed == LEFT: 
-		# move left
-		command.angular.z = SHIFT
-    elif keyPressed == RIGHT: 
-		# move right
-		command.angular.z = -1 * SHIFT
+	command.angular.z = 0.0
+        command.linear.x = 0.0
+
+    # forward
+    FORWARD_LIMIT = 0.8
+    if keyPressed == UP && targetX > 0:
+	command.linear.x = min(targetX, FORWARD_LIMIT)
+	
+    # backward
+    if keyPressed == DOWN && targetX < 0:
+	command.linear.x = max(targetX, -1 * FORWARD_LIMIT)
+   
+    # turn left
+    Z_LIMIT = 1.0
+    if keyPressed == LEFT && targetZ > 0:
+	command.angular.z = max(targetZ, Z_LIMIT)
+	
+    # turn right
+    if keyPressed == RIGHT && targetZ < 0:
+	command.angular.z = min(targetZ, -1 * Z_LIMIT)
+    
+   
+	
+#     elif keyPressed == UP：
+# 		# move forward with constant_command.py
+# 		# something like .... constant_command.publish(0)
+# 		# as button is pressed, accelerate
+# 	command.linear.x = SHIFT
+#     elif keyPressed == DOWN:
+# 		# move backwards
+# 	command.linear.x = -1 * SHIFT
+#     elif keyPressed == LEFT: 
+# 		# move left
+# 	command.angular.z = SHIFT
+#     elif keyPressed == RIGHT: 
+# 		# move right
+#     	command.angular.z = -1 * SHIFT
 
 def dxCallback(data)
     targetX = data
@@ -47,6 +91,15 @@ def keyController():
     rospy.Subscriber("/keys", Int32, keyCallback)	# subscribe to pub_keys
     rospy.Subscriber("/dx", Float32, dxCallback)	# subscribe to pub_dx
     rospy.Subscriber("/dz", Float32, dzCallback)	# subscribe to pub_dz
+	
+    rospy.on_shutdown(cleanUp)
+
+    while not rospy.is_shutdown():
+        if dirty:
+            dirty = False
+            update_command()
+            pub_ctrl.publish(command)
+	rospy.sleep(0.05)
     rospy.spin()
 	
 if __name__ == '__main__':
