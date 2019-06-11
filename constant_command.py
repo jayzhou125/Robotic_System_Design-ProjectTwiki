49a6f77257d77c4f1e063247431d716102a3bc0c
jayzhou125 <33584805+jayzhou125@users.noreply.github.com>
2019-06-11 10:29:42 -0400

    Update constant_command.py

--- constant_command.py
+++ constant_command.py
@@ -95,8 +95,8 @@ def smooth():
 	pub_velocity.publish(zero())
         return
     
-    DELTA_X = 0.03
-    DELTA_Z = 0.35 # was 0.4
+    DELTA_X = 0.04 # was 0.3
+    DELTA_Z = 0.4 # was 0.4
 
     # smooth x
     t = targetCommand.linear.x
