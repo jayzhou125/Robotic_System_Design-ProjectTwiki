7742f87adf808e0c939851e9beb6685b66f2348b
Alexis Sanders <50931199+als429@users.noreply.github.com>
2019-06-24 18:27:52 -0400

    adding single quotes

--- examples/display-an-image-for-one-second.py
+++ examples/display-an-image-for-one-second.py
@@ -1,8 +1,8 @@
 import cv2
 
 def main():
-  color_image = cv2.imread(start_img.jpg, cv2.IMREAD_COLOR)
-  cv2.imshow(Image, color_image)
+  color_image = cv2.imread('start_img.jpg', cv2.IMREAD_COLOR)
+  cv2.imshow('Image', color_image)
   cv2.waitKey(1000)
   cv2.destroyAllWindows()
   
