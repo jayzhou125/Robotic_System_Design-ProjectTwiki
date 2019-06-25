d6ab10034755ab364adb70748eab863e7030fb22
Alexis Sanders <50931199+als429@users.noreply.github.com>
2019-06-25 10:09:48 -0400

    Create in-range.py

---
+++ examples/in-range.py
@@ -0,0 +1,66 @@
+import cv2
+
+import numpy as np
+blur_image = None
+mask_image = None
+min_color = None
+max_color = None
+first_color = True
+
+def mouseEvent(event, x, y, flags, param):
+  global blur_image, min_color, max_color, first_color, mask_image
+  if event == 4:
+    blue = blur_image[y][x][0]
+    green = blur_image[y][x][1]
+    red = blur_image[y][x][2]
+    
+  if first_color:
+    min_color = [blue, green, red]
+    max_color = [blue, green, red]
+    lower_bound = np.array(min_color, dtype = 'uint8')
+    upper_bound = np.array(max_color, dtype = 'uint8')
+    mask_image = cv2.inRange(blur_image, lower_bound, upper_bound)
+    first_color = False
+  else:
+    [min_blue, min_green, min_red] = min_color
+    [max_blue, max_green, max_red] = max_color
+
+    if blue < min_blue:
+      min_blue = blue
+    if green < min_green:
+      min_green = green
+    if red < min_red:
+      min_red = red
+    if blue > max_blue:
+      max_blue = blue
+    if green > max_green:
+      max_green = green
+    if red > max_red:
+      max_red = red
+     
+    min_color = [min_blue, min_green, min_red]
+    max_color = [max_blue, max_green, max_red]
+    lower_bound = np.array(min_color, dtype = 'uint8')
+    upper_bound = np.array(max_color, dtype = 'uint8')
+    mask_image = cv2.inRange(blur_image, lower_bound, upper_bound)
+
+    print 'min_color:', min_color, 'max_color:', max_color
+    
+def main():
+  global blur_image, min_color, max_color, first_color, mask_image
+  color_image = cv2.imread('../start_img.jpg', cv2.IMREAD_COLOR)
+  blur_image = cv2.GaussianBlur(color_image, (5, 5), 0)
+
+  cv2.namedWindow('Blur Image')
+  cv2.setMouseCallback('Blur Image', mouseEvent)
+  cv2.imshow('Blur Image', blur_image)
+  while True:
+    if not first_color:
+      cv2.imshow('Mask', mask_image)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+      break;
+  cv2.destroyAllWindows()
+  
+if __name__ == '__main__':
+  main()
