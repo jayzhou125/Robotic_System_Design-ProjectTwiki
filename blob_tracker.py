fa0a684bff31bf3949ac3472be4133bd5099ea14
= <kmz25@pitt.edu>
2019-07-02 18:00:13 -0400

    fix turning?

--- blob_tracker.py
+++ blob_tracker.py
@@ -36,10 +36,10 @@ def track_blobs():
 
         print(centerOffset)
         if centerOffset > 20:
-            command.angular.z = max(-Z_MAX, centerOffset/rawBlobs.image_width)
+            command.angular.z = min(Z_MAX, centerOffset/rawBlobs.image_width)
             
         elif centerOffset < -20:
-            command.angular.z = min(Z_MAX, centerOffset/rawBlobs.image_width)
+            command.angular.z = max(-Z_MAX, centerOffset/rawBlobs.image_width)
             
         pub_command.publish(command)
     
