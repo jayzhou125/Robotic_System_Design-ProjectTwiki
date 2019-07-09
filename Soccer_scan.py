530bae20d7288324dcda8f10faaf044ba860a190
jayzhou125 <33584805+jayzhou125@users.noreply.github.com>
2019-07-09 17:03:35 -0400

    Create Soccer_scan.py

---
+++ Soccer_scan.py
@@ -0,0 +1,5 @@
+# This is a node for all the scan functions
+# This node will return three value： 
+#     the angle which the ball is found, 
+#     the angle which the goal is found, 
+#     the direaction of the turn before the second scan.
