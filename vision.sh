c1036285a15cd10c1bdc3126a69c4637f88b5ad0
student <student@twiki.(none)>
2019-07-09 19:40:23 -0400

    separate soccer and line colors and launch files

--- vision.sh
+++ vision.sh
@@ -8,7 +8,7 @@ gnome-terminal --tab --title="kobuki" -e "bash -c 'roslaunch kobuki_node minimal
 
 gnome-terminal --tab --title="gscam_launch" -e "bash -c 'roslaunch gscam v4l.launch'"
 
-gnome-terminal --tab --title="cmvision_launch" -e "bash -c 'roslaunch mypackage cmvision.launch'"
+gnome-terminal --tab --title="cmvision_launch" -e "bash -c 'roslaunch mypackage soccer_vision.launch'"
 
 gnome-terminal --tab --title="blob_tracker" -e "bash -c 'rosrun mypackage blob_tracker.py';bash"
 
