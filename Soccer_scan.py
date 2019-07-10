e6dd43a5bd1b40ccd6b5e16eff774d1be2bd4577
jayzhou125 <33584805+jayzhou125@users.noreply.github.com>
2019-07-09 21:14:18 -0400

    Update Soccer_scan.py

--- Soccer_scan.py
+++ Soccer_scan.py
@@ -28,33 +28,72 @@ from cmvision.msg import Blobs, Blob
 from sensor_msgs.msg import Image
 import location
 
-pub_command = rospy.Publisher("/kobuki_command", Twist, queue_size=10)  # publish command
-pub_stop = rospy.Publisher("/emergency_stop", Empty, queue_size=10)     # publish stop
-
-# initialization
 rawBlobs = Blobs()
 mergedBlobs = Blobs()
 width = 0
 
-def init():
-    rospy.init_node("blob_tracker")     # initialize the node
-    location.init()                     # init the location
-    rospy.on_shutdown(cleanUp)          
-    rospy.Subscriber("/blobs", Blobs, setRawBlobs)  # subscribe to blobs
 
-def track_blobs():
+def turn_left(speed)
+    command.angular.z = speed
+    pub_command.publish(command)    # publish the twist command to the kuboki node
+
+# stop the robot
+def zero():
+    result = Twist();
+    result.angular.z = 0
+    result.linear.x = 0
+
+    return result
+
+# keep turning left until the ball and the goal is find
+def scan():
     global rawBlobs, pub_command
+    
+    track_blobs("ball")
+    ball_angle = record_location()
+    
+    # record location
+    
+    track_blobs("goal")
+    goal_angle = record_location()
+    
+    return ball_angle, goal_angle
+
+def track_blobs(mode):
+    global rawBlobs, pub_command, ballNotFound, goalNotFound
 
     Z_MAX = 0.5  # maximum speed
 
     while(True):
+       
         command = zero() 
-        trackingBlob = mergeBlobs()  # get the bolb to follow
-        center = rawBlobs.image_width//2    # the center of the image
-        centerOffset = center - trackingBlob.x  # the offset that the ball need to travel 
-        if trackingBlob.x == 0 and trackingBlob.y == 0:     # no bolb is found
+        mergedBlobs = mergeBlobs()
+        trackingBlob = None
+
+        print mergedBlobs.keys()
+
+        if mode == "ball" and "blueball" in mergedBlobs.keys():
+            trackingBlob = mergedBlobs["blueball"][0]
+        elif mode == "goal" and "pinkgoal" in mergedBlobs.keys() and "yellowgoal" in mergedBlobs.keys():
+            for outer in mergedBlobs["yellowgoal"]:
+                for inner in mergedBlobs["pinkgoal"]:
+                    if (inner.left >= outer.left
+                    and inner.right <= outer.right
+                    and inner.top >= outer.top
+                    and inner.bottom <= outer.bottom):
+                        trackingBlob = inner
+                        break
+                
+                if trackingBlob is not None:
+                    break
+                        
+        if trackingBlob is None:
+            turn_left()
             continue
 
+        center = rawBlobs.image_width//2    # the center of the image
+        centerOffset = center - trackingBlob.x  # the offset that the ball need to travel 
+        
         speed = 4 * centerOffset/float(rawBlobs.image_width)    # calculate the right amount of speed for the command
 
         print "Tracking Blob Object Attr: ", trackingBlob.name, "<<" # added AS
@@ -65,58 +104,73 @@ def track_blobs():
         elif centerOffset < -20:    # if the offset is smaller than -20
             command.angular.z = max(-Z_MAX, speed)  # turn right and follow the ball
             # print([command.angular.z, centerOffset/rawBlobs.image_width])
+        else
+#             ballNotFound = False
+            command = zero()
+            # stop the robot
+            pub_command.publish(command)    # publish the twist command to the kuboki nod
+            return 
         
         pub_command.publish(command)    # publish the twist command to the kuboki node
 
-def setRawBlobs(blobs):
-    global rawBlobs
-    rawBlobs = blobs
 
-# stop the robot
-def zero():
-    result = Twist();
-    result.angular.z = 0
-    result.linear.x = 0
+def record_location()
+    # record odom
+    _, _, angle = location.currentLocation
+    return angle
+    
 
-    return result
 
+# merge bolbs
 def mergeBlobs():
     global rawBlobs
-    x = 0;
-    y = 0;
-    left = 0;
-    right = 0;
-    top = 0;
-    bottom = 0;
-    area = 0;
-    name = ""; # added AS
+
+    merged = {}
+
     for b in rawBlobs.blobs:
-        x = x + (b.x * b.area)
-        y = y + (b.y * b.area)
-        left = left + (b.left * b.area)
-        right = right + (b.right * b.area)
-        top = top + (b.top * b.area)
-        bottom = bottom + (b.bottom * b.area)
-        area = area + b.area
-	name = b.name # added AS
-    result = Blob()
+        mergeTarget = Blob()
+        mergeNeeded = False
+        
+        #check to see if there is an existing blob to merge with
+        if b.name in merged.keys(): 
+            for m in merged[b.name]:
+                if overlaps(b, m):
+                    mergeTarget = m
+                    mergeNeeded = True
+                    break
+        
+        else: # no blobs by that name
+            merged[b.name] = []
+        
+        # merge
+        if not mergeNeeded:
+            merged[b.name].append(b)
+        else: # merge needed
+            mergeTarget.left = min(mergeTarget.left, b.left)
+            mergeTarget.right = max(mergeTarget.right, b.right)
+            mergeTarget.top = min(mergeTarget.top, b.top)
+            mergeTarget.bottom = max(mergeTarget.bottom, b.bottom)
+            mergeTarget.area = (mergeTarget.right - mergeTarget.left) * (mergeTarget.bottom - mergeTarget.top)
+    
+    for m in merged.keys():
+        merged[m].sort(key=lambda x: x.area, reverse=True)
+    
+    return merged
 
-    if area > 0:
-        result.x = x / area
-        result.y = y / area
-        result.left = left / area
-        result.right = right / area
-        result.top = top / area
-        result.bottom = bottom / area
-        result.area = x * y
-	result.name = name # added AS; what gets assigned to trackingBlob
+# find the overlap blob
+def overlaps(blob1, blob2):
+    h_over = False
+    v_over = True
+    if (blob1.left > blob2.left and blob2.left < blob2.right
+    or  blob1.right > blob2.left and blob1.right < blob2.right
+    or  blob2.left > blob1.left and blob2.left < blob1.right
+    or  blob2.right > blob1.left and blob2.right < blob1.right):
+        h_over = True
+    
+    if (blob1.top > blob2.top and blob2.top < blob2.bottom
+    or  blob1.bottom > blob2.top and blob1.bottom < blob2.bottom
+    or  blob2.top > blob1.top and blob2.top < blob1.bottom
+    or  blob2.bottom > blob1.top and blob2.bottom < blob1.bottom):
+        v_over = True
 
-    # print "blob merged center is + (", x, ", ", y, ")"
-    return result
-
-def cleanUp():
-    pub_stop.publish(Empty())
-
-if __name__ == "__main__":
-    init()
-    track_blobs()
+    return h_over and v_over
