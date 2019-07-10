3e1fc5ec091de223bb7c48048879cba9b989b154
student <student@twiki.(none)>
2019-07-09 21:37:50 -0400

    vision.sh updates

--- vision.sh
+++ vision.sh
@@ -8,8 +8,8 @@ gnome-terminal --tab --title="kobuki" -e "bash -c 'roslaunch kobuki_node minimal
 
 gnome-terminal --tab --title="gscam_launch" -e "bash -c 'roslaunch gscam v4l.launch'"
 
-gnome-terminal --tab --title="soccer_node" -e "bash -c 'rosrun mypackage soccer_node.py';bash"
+gnome-terminal --tab --title="soccer_vision" -e "bash -c 'roslaunch mypackage soccer_vision.launch'"
 
 gnome-terminal --tab --title="constant_command" -e "bash -c 'rosrun mypackage constant_command.py'"
 
-gnome-terminal --tab --title="soccer_vision" -e "bash -c 'roslaunch mypackage soccer_vision.launch'"
+gnome-terminal --tab --title="soccer_node" -e "bash -c 'rosrun mypackage soccer_node.py';bash"
