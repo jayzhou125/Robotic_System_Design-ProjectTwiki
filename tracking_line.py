2fd437ca55ae4db1befb8de83c340db2f3055000
student <student@twiki.(none)>
2019-07-09 21:34:29 -0400

    kevin updates

--- tracking_line.py
+++ tracking_line.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 import rospy
 from std_msgs.msg import Empty
 from geometry_msgs.msg import Twist
@@ -8,67 +7,37 @@ from cmvision.msg import Blobs, Blob
 from sensor_msgs.msg import Image
 import location
 
+pub_command = rospy.Publisher("/kobuki_command", Twist, queue_size=10)  # publish command
+pub_stop = rospy.Publisher("/emergency_stop", Empty, queue_size=10)     # publish stop
+
+# initialization
 rawBlobs = Blobs()
-mergedBlobs = Blobs()
+mergedBlobs = {}
 width = 0
 
+def init():
+    rospy.init_node("tracking_line")     # initialize the node
+    location.init()                     # init the location
+    rospy.on_shutdown(cleanUp)          
+    rospy.Subscriber("/blobs", Blobs, setRawBlobs)  # subscribe to blobs
 
-def turn_left(speed):
-    command.angular.z = speed
-    pub_command.publish(command)    # publish the twist command to the kuboki node
-
-# stop the robot
-def zero():
-    result = Twist();
-    result.angular.z = 0
-    result.linear.x = 0
-
-    return result
-
-# keep turning left until the ball and the goal is find
-def scan():
+def track_blobs():
     global rawBlobs, pub_command
-    
-    track_blobs("ball")
-    ball_angle = record_location()
-    
-    # record location
-    
-    track_blobs("goal")
-    goal_angle = record_location()
-    
-    return ball_angle, goal_angle
-
-def track_blobs(mode):
-    global rawBlobs, pub_command, ballNotFound, goalNotFound
 
     Z_MAX = 0.5  # maximum speed
 
     while(True):
-       
-        command = zero() 
+        command = zero()
+        command.linear.x = 0.2
         mergedBlobs = mergeBlobs()
         trackingBlob = None
 
         print mergedBlobs.keys()
 
-        if mode == "ball" and "blueball" in mergedBlobs.keys():
-            trackingBlob = mergedBlobs["blueball"][0]
-        elif mode == "goal" and "pinkgoal" in mergedBlobs.keys() and "yellowgoal" in mergedBlobs.keys():
-            for outer in mergedBlobs["yellowgoal"]:
-                for inner in mergedBlobs["pinkgoal"]:
-                    if (inner.left >= outer.left
-                    and inner.right <= outer.right
-                    and inner.top >= outer.top
-                    and inner.bottom <= outer.bottom):
-                        trackingBlob = inner
-                        break
-                
-                if trackingBlob is not None:
-                    break
+        if "greenline" in mergedBlobs.keys():
+            trackingBlob = mergedBlobs["greenline"][0]
                         
         if trackingBlob is None:
-            turn_left()
             continue
 
         center = rawBlobs.image_width//2    # the center of the image
@@ -84,23 +53,46 @@ def track_blobs(mode):
         elif centerOffset < -20:    # if the offset is smaller than -20
             command.angular.z = max(-Z_MAX, speed)  # turn right and follow the ball
             # print([command.angular.z, centerOffset/rawBlobs.image_width])
-        else:
-            command = zero()
-            # stop the robot
-            pub_command.publish(command)    # publish the twist command to the kuboki nod
-            return 
         
         pub_command.publish(command)    # publish the twist command to the kuboki node
 
+def setRawBlobs(blobs):
+    global rawBlobs
+    rawBlobs = blobs
 
-def record_location():
-    # record odom
-    _, _, angle = location.currentLocation
-    return angle
-    
+# stop the robot
+def zero():
+    result = Twist();
+    result.angular.z = 0
+    result.linear.x = 0
 
+    return result
 
-# merge bolbs
+# def mergeBlobs():
+#     # try out the area param simple detection
+#     global rawBlobs
+#     result = Blob()
+
+#     params = cv2.SimpleBlobDetector_Params()
+# 	# params.minThreshold = 0
+# 	# params.maxThreshold = 255
+# 	# params.filterByArea = True
+#     params.minArea = 0.01 * rawBlobs.image_width * rawBlobs.image_height # tenth of the image size
+#     params.maxArea = rawBlobs.image_width * rawBlobs.image_height
+# 	# params.filterByCircularity = True
+# 	# params.minCircularity = 0.1
+# 	# params.filterByConvexity = False
+# 	# params.minConvexity = 0.9
+# 	# params.filterByInertia = False
+# 	# params.minInertiaRatio = 0.5
+#     ver = (cv2.__version__).split('.')
+#     if int(ver[0]) < 3:
+#         result = cv2.SimpleBlobDetector(params)
+#     else:
+#     	result = cv2.SimpleBlobDetector_create(params)
+
+#     return result
+
 def mergeBlobs():
     global rawBlobs
 
@@ -136,7 +128,7 @@ def mergeBlobs():
     
     return merged
 
-# find the overlap blob
+
 def overlaps(blob1, blob2):
     h_over = False
     v_over = True
@@ -153,3 +145,44 @@ def overlaps(blob1, blob2):
         v_over = True
 
     return h_over and v_over
+            
+
+#     global rawBlobs
+#     x = 0;
+#     y = 0;
+#     left = 0;
+#     right = 0;
+#     top = 0;
+#     bottom = 0;
+#     area = 0;
+#     name = ""; # added AS
+#     for b in rawBlobs.blobs:
+#         x = x + (b.x * b.area)
+#         y = y + (b.y * b.area)
+#         left = left + (b.left * b.area)
+#         right = right + (b.right * b.area)
+#         top = top + (b.top * b.area)
+#         bottom = bottom + (b.bottom * b.area)
+#         area = area + b.area
+# 	name = b.name # added AS
+#     result = Blob()
+
+#     if area > 0:
+#         result.x = x / area
+#         result.y = y / area
+#         result.left = left / area
+#         result.right = right / area
+#         result.top = top / area
+#         result.bottom = bottom / area
+#         result.area = x * y
+# 	result.name = name # added AS; what gets assigned to trackingBlob
+
+#     # print "blob merged center is + (", x, ", ", y, ")"
+#     return result
+
+def cleanUp():
+    pub_stop.publish(Empty())
+
+if __name__ == "__main__":
+    init()
+    track_blobs()
