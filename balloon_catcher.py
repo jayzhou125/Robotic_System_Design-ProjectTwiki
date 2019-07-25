0141cfa6f03a586f4909f9916efc1d0ff384e4ac
student <student@twiki.(none)>
2019-07-25 16:38:30 -0400

    update balloon stuff

--- balloon_catcher.py
+++ balloon_catcher.py
@@ -1,1 +1,33 @@
 #!/usr/bin/env python
+
+import rospy
+import location
+import pid
+from std_msgs.msg import Empty
+from geometry_msgs.msg import Twist
+from cmvision.msg import Blobs, Blob
+from sensor_msgs.msg import Image
+from batch_controller import execute, cancel
+
+
+pub_command = rospy.Publisher("/kobuki_command", Twist, queue_size=10)  # publish command
+pub_stop = rospy.Publisher("/emergency_stop", Empty, queue_size=10)   # publish stop
+
+##THOUGHTS:
+# we probably need the pid to keep the balloon in the center of the screen
+# We also might need to move quickly to the pid (sharpen the movement)
+
+def catcher():
+
+	# get the angle of kinect
+
+	# get the balloon in the center of the screen(ball_tracker)
+
+	# get the distance to the balloon 
+
+	# calculate the forward distance to catch the balloon
+
+	# move the calculated distance 
+
+
+
