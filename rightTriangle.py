ecdf15a62ce9c2c3c49a091b0ad1b3de3aa2d17a
Alexis Sanders <50931199+als429@users.noreply.github.com>
2019-07-25 16:53:47 -0400

    update to methods

--- rightTriangle.py
+++ rightTriangle.py
@@ -2,14 +2,28 @@
 
 from math import sin, cos, radians
 
+theta = 0.0
+h = 0.0 
+
+'''
 class rightTriangle():
     def __init__(self, theta, h):
 
         self.theta = theta
         self.h = h
         self.alpha = 90 - h
-		self.o = sin(radian(theta))* h
-		self.a = cos(radian(theta))* h
+	self.o = sin(radian(theta))* h
+	self.a = cos(radian(theta))* h
 
     def getAdjacent(self):
         return a
+'''
+
+def getOpposite(theta, h):
+    return sin(radian(theta))* h
+
+def getAdjacent(theta, h):
+    return cos(radian(theta))* h
+
+def getAlpha(h):
+    return 90 - h
