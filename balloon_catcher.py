19e14f68b309789fa3be46fe3b1e68ab1d262a3d
= <kmz25@pitt.edu>
2019-07-31 17:02:50 -0400

    fix indentations

--- balloon_catcher.py
+++ balloon_catcher.py
@@ -27,27 +27,27 @@ depth_available = False
 
 # get the angle of kinect
 def angleCallback(data):
-	global kinect_angle
-	kinect_angle = data.data
+    global kinect_angle
+    kinect_angle = data.data
 
 def depthCallback(data):
-	global depth_map, depth_available
-	depth_map = data
-	depth_available = True
+    global depth_map, depth_available
+    depth_map = data
+    depth_available = True
 
 
 def getDepth(x, y):
-	global depth_map
-	data = depth_map.data
-	offset = (depth_map.step * y) + (4 * x)
+    global depth_map
+    data = depth_map.data
+    offset = (depth_map.step * y) + (4 * x)
         print "depth map size {}, offset {}:{}".format(len(data), offset, offset+3)
-	dist = unpack('f', depth_map.data[offset] + depth_map.data[offset+1] + depth_map.data[offset+2] + depth_map.data[offset+3])
-	return dist[0]
+    dist = unpack('f', depth_map.data[offset] + depth_map.data[offset+1] + depth_map.data[offset+2] + depth_map.data[offset+3])
+    return dist[0]
 
 def cleanUp():
-	global pub_stop
-	pub_stop.publish(Empty())
-	
+    global pub_stop
+    pub_stop.publish(Empty())
+    
 # stop the robot
 def zero():
     result = Twist();
@@ -57,96 +57,97 @@ def zero():
     return result
 
 def catcher():
-	global pub_stop, pub_command, kinect_angle, depth_available
+    global pub_stop, pub_command, kinect_angle, depth_available
 
-	rospy.init_node("balloon_catcher")
-	rospy.Subscriber("/cur_tilt_angle", Float64, angleCallback)
-	rospy.Subscriber("/camera/depth/image", Image, depthCallback)
+    rospy.init_node("balloon_catcher")
+    rospy.Subscriber("/cur_tilt_angle", Float64, angleCallback)
+    rospy.Subscriber("/camera/depth/image", Image, depthCallback)
         location.init()
-	rospy.on_shutdown(cleanUp)
+    rospy.on_shutdown(cleanUp)
 
         while not depth_available:
-        	rospy.sleep(0.1)
+            rospy.sleep(0.1)
 
-	# get the balloon in the center of the screen(ball_tracker)
-	targetBlob = scan(pub_command)
-	
-	KINECT_PIXELS_PER_DEGREE = 10
-	
-	dist = None
-	horizontal = 0
-	vertical = 0
-	v_prev = 0
-	V_THRESHOLD = 0.02 # 2cm
+    # get the balloon in the center of the screen(ball_tracker)
+    targetBlob = scan(pub_command)
+    
+    KINECT_PIXELS_PER_DEGREE = 10
+    
+    dist = None
+    horizontal = 0
+    vertical = 0
+    v_prev = 0
+    V_THRESHOLD = 0.02 # 2cm
         
     while v_prev - vertical >= V_THRESHOLD:
-		rospy.sleep(0.01)
-		print (v_prev, vertical, v_prev - vertical)
-		centerOffset, targetBlob = get_blob_offset()
+        rospy.sleep(0.01)
+        print (v_prev, vertical, v_prev - vertical)
+        centerOffset, targetBlob = get_blob_offset()
 
-		if centerOffset == -1 or targetBlob is None:
-			continue
+        if centerOffset == -1 or targetBlob is None:
+            continue
 
-		# calculate angle from ground to camera-balloon line
-		angle = kinect_angle + ((depth_map.height/2) - targetBlob.y)/KINECT_PIXELS_PER_DEGREE
+        # calculate angle from ground to camera-balloon line
+        angle = kinect_angle + ((depth_map.height/2) - targetBlob.y)/KINECT_PIXELS_PER_DEGREE
 
-		# get the distance to the balloon 
-		dist = getDepth(targetBlob.x, targetBlob.y)
+        # get the distance to the balloon 
+        dist = getDepth(targetBlob.x, targetBlob.y)
 
-		v_prev = vertical
-		vertical = getOpposite(angle, dist)
-		horizontal = getAdjacent(angle, dist)
+        v_prev = vertical
+        vertical = getOpposite(angle, dist)
+        horizontal = getAdjacent(angle, dist)
 
-	print "balloon falling {} meters from camera".format(dist)
-	print "height {} meters, estimated landing point {} meters".format(vertical, horizontal)
+    print "balloon falling {} meters from camera".format(dist)
+    print "height {} meters, estimated landing point {} meters".format(vertical, horizontal)
 
-	# move the calculated distance 
-	# we could use batch command's execute but the trick will be the ball won't be in the center all the time when it falls
-	# so we might need to use the tracking blob with a higher sensitivity.
+    # move the calculated distance 
+    # we could use batch command's execute but the trick will be the ball won't be in the center all the time when it falls
+    # so we might need to use the tracking blob with a higher sensitivity.
 
-	location.resetOdom()
-	command = zero()
-	SLEEP = 0.01
-	DELTA_X = 0.9 * SLEEP
-        Z_MAX = 0.6
-        IMAGE_WIDTH = 640
-	X_TURN_MAX = 0.7
-	while(location.currentLocation[0] < horizontal):
-		if command.linear.x < 1:
-			command.linear.x += DELTA_X
-		if command.linear.x > 1:
-			command.linear.x = 1
-		
+    location.resetOdom()
+    command = zero()
+    SLEEP = 0.01
+    DELTA_X = 0.9 * SLEEP
+    Z_MAX = 0.6
+    IMAGE_WIDTH = 640
+    X_TURN_MAX = 0.7
+    while(location.currentLocation[0] < horizontal):
+        if command.linear.x < 1:
+            command.linear.x += DELTA_X
+        if command.linear.x > 1:
+            command.linear.x = 1
+        
         command.angular.z = 0
 
         centerOffset, _ = get_blob_offset()
-	    if centerOffset is None:
-			pub_command.publish(command)
-			continue
-	    
-		speed = 50 * centerOffset/float(IMAGE_WIDTH)    # calculate the right amount of speed for the command
+        if centerOffset is None:
+            pub_command.publish(command)
+            continue
+        
+        speed = 50 * centerOffset/float(IMAGE_WIDTH)    # calculate the right amount of speed for the command
 
-		
-		if centerOffset > 20:   # if the offset is bigger than 20
-			# print "{} LEFT".format(abs(centerOffset))
-			command.angular.z = min(Z_MAX, speed) # turn left and follow
-			# print([command.angular.z, centerOffset/rawBlobs.image_width])
-		elif centerOffset < -20:    # if the offset is smaller than -20
-			# print "{} RIGHT".format(abs(centerOffset))
-			command.angular.z = max(-Z_MAX, speed)  # turn right and follow the ball
-			# print([command.angular.z, centerOffset/rawBlobs.image_width])
+        
+        if centerOffset > 20:   # if the offset is bigger than 20
+            # print "{} LEFT".format(abs(centerOffset))
+            command.angular.z = min(Z_MAX, speed) # turn left and follow
+            # print([command.angular.z, centerOffset/rawBlobs.image_width])
+        elif centerOffset < -20:    # if the offset is smaller than -20
+            # print "{} RIGHT".format(abs(centerOffset))
+            command.angular.z = max(-Z_MAX, speed)  # turn right and follow the ball
+            # print([command.angular.z, centerOffset/rawBlobs.image_width])
 
-		if abs(command.angular.z) > 0.2 and command.linear.x > X_TURN_MAX:
-			command.linear.x = X_TURN_MAX
-			print "turning"
-		pub_command.publish(command)
-		print "x {}, z {}".format(command.linear.x, command.angular.z)
-		rospy.sleep(SLEEP)
+        if abs(command.angular.z) > 0.2 and command.linear.x > X_TURN_MAX:
+            command.linear.x = X_TURN_MAX
+            print "turning"
 
-	command = zero()
-	pub_command.publish(command)
-	
-	
+        pub_command.publish(command)
+        print "x {}, z {}".format(command.linear.x, command.angular.z)
+        rospy.sleep(SLEEP)
+
+    command = zero()
+    pub_command.publish(command)
+    
+    
 # def track_blobs():
 #     global rawBlobs, pub_command, pub_stop
 
@@ -254,4 +255,4 @@ def catcher():
 #     return h_over and v_over
 
 if __name__ == "__main__":
-	catcher()
+    catcher()
